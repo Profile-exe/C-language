aaafdd1714cd2e258d195dba9942365460f83a49
MyeongDeok-Kang <68066437+Profile-exe@users.noreply.github.com>
2020-08-18 00:32:37 +0900

    Update get max[tournament].c
    Add other solution.

--- 독하게 시작하는 C 프로그래밍/get max[tournament].c
+++ 독하게 시작하는 C 프로그래밍/get max[tournament].c
@@ -8,6 +8,12 @@ int main(void)
 	scanf_s("%d %d %d", &a, &b, &c);
 	max = a > b ? (a > c ? a : c) : (b > c ? b : c);
 
+	/*
+	max = a;
+	max = b > max ? b : max;
+	max = c > max ? c : max;
+	*/
+
 	printf("MAX : %d\n", max);
 	return 0;	
 }
