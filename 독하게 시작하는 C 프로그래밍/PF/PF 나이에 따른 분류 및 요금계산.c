79db1653a7bb0e95f040cadd0590ec4abd41a3cd
MyeongDeok-Kang <68066437+Profile-exe@users.noreply.github.com>
2020-08-19 00:39:22 +0900

    Update PF 나이에 따른 분류 및 요금계산.c
    The value of each case is adjusted by adding a variable called 'Rate' and finally output it.

--- 독하게 시작하는 C 프로그래밍/PF/PF 나이에 따른 분류 및 요금계산.c
+++ 독하게 시작하는 C 프로그래밍/PF/PF 나이에 따른 분류 및 요금계산.c
@@ -1,19 +1,20 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-	int age;
-	double fee = 1000;
+	int fee = 1000, age = 0;
+	double rate = 0.0;
 
 	scanf_s("%d", &age);
-	if (age > 13) {
-		if (age < 20) printf("최종요금 : %.0f원", fee * 0.75);
-		else printf("최종요금 : %.0f원", fee);
+	if (age < 14) {
+		if (age < 4)	rate = 0.0;
+		else			rate = 0.5;
 	}
 	else {
-		if (age < 4) printf("최종요금 : %.0f원", fee * 0);
-		else printf("최종요금 : %.0f원", fee * 0.5);
+		if (age < 20)	rate = 0.75;
+		else			rate = 1.0;
 	}
 
+	printf("최종요금 : %d원\n", (int)(fee * rate));
 	return 0;
 }
