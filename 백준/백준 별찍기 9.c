066fa6f70a0c0264f92aa009c0dfbb2ff8b9fefd
MyeongDeok-Kang <68066437+Profile-exe@users.noreply.github.com>
2020-08-19 20:09:06 +0900

    Change more intuitively

--- 백준/백준 별찍기 9.c
+++ 백준/백준 별찍기 9.c
@@ -9,14 +9,14 @@ int main(void)
 	for (i = 0; i < 2 * n - 1; i++) {
 		if (i < n) {
 			for (j = n; j > n - i; j--)
-				printf(" ");			
-			for (j = 0; j < 2 * n - 1 - 2 * i; j++)
+				printf(" ");
+			for (j = 0; j < 2 * (n - i) - 1; j++)
 				printf("*");
 		}
 		else if (i >= n) {
 			for (j = 0; j < 2 * n - 2 - i; j++)
 				printf(" ");
-			for (j = 2 * n - 1; j > 2 * n - 2 * i + 2 * (n - 2); j--) {
+			for (j = 2 * n - 1; j > 2 * (n - i) + 2 * (n - 2); j--) {
 				printf("*");
 			}
 		}
