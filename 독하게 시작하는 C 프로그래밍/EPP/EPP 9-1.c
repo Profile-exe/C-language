235ae72894875dcbf7daf9f2bcb5ce92c189da85
MyeongDeok-Kang <68066437+Profile-exe@users.noreply.github.com>
2020-08-26 22:34:05 +0900

    Update EPP 9-1.c
    Change the condition statement of the repeating statement to work regardless of the size of the array

--- 독하게 시작하는 C 프로그래밍/EPP/EPP 9-1.c
+++ 독하게 시작하는 C 프로그래밍/EPP/EPP 9-1.c
@@ -5,8 +5,8 @@ int main(void)
 	int aList[5] = { 30, 40, 10, 50, 20 };
 	int i = 0, j = 0, nTmp = 0;
 
-	for (i = 0; i < 4; i++) {
-		for (j = i + 1; j < 5; j++) {
+	for (i = 0; i < sizeof(aList)/sizeof(int) - 1; i++) {
+		for (j = i + 1; j < sizeof(aList)/sizeof(int); j++) {
 			if (aList[i] > aList[j]) {
 				nTmp = aList[i];
 				aList[i] = aList[j];
