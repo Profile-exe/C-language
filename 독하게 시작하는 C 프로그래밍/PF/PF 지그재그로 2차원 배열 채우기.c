1f841f22a08d58292fb39f352788cc832d5308e3
MyeongDeok-Kang <68066437+Profile-exe@users.noreply.github.com>
2020-08-28 03:51:39 +0900

    Create PF 지그재그로 2차원 배열 채우기.c

---
+++ 독하게 시작하는 C 프로그래밍/PF/PF 지그재그로 2차원 배열 채우기.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[5][5] = { 0, };
+	int i, j, count = 0;
+
+	for (i = 0; i < 5; i++) {
+		if (i % 2 != 0) {
+			for (j = 4; j >= 0; j--)
+				arr[i][j] = ++count;
+		}
+		else {
+			for (j = 0; j < 5; j++)
+				arr[i][j] = ++count;
+		}
+	}
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5; j++)
+			printf("%d\t", arr[i][j]);
+		putchar('\n');
+	}
+
+	return 0;
+}
