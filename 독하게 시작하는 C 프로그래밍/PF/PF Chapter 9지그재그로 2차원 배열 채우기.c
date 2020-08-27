8140b43e33614cc34519a14ec9f958e2e3be5576
MyeongDeok-Kang <68066437+Profile-exe@users.noreply.github.com>
2020-08-28 06:00:46 +0900

    Update PF Chapter 9지그재그로 2차원 배열 채우기.c
    Insert replaceable repeating statement code into annotation

--- 독하게 시작하는 C 프로그래밍/PF/PF Chapter 9지그재그로 2차원 배열 채우기.c
+++ 독하게 시작하는 C 프로그래밍/PF/PF Chapter 9지그재그로 2차원 배열 채우기.c
@@ -6,13 +6,13 @@ int main(void)
 	int i, j, count = 0;
 
 	for (i = 0; i < 5; i++) {
-		if (i % 2 != 0) {
-			for (j = 4; j >= 0; j--)
+		if (i % 2 == 0) {
+			for (j = 0; j < 5; j++)
 				arr[i][j] = ++count;
 		}
 		else {
-			for (j = 0; j < 5; j++)
-				arr[i][j] = ++count;
+			for (j = 4; j >= 0; j--)	//for (j = 0; j < 5; j++)
+				arr[i][j] = ++count;	//    arr[i][4-j] = ++count;
 		}
 	}
 	for (i = 0; i < 5; i++) {
