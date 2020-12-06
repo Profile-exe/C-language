ef74bb855240689c370d3ae7bfd13d5ab0072bd2
MyeongDeok-Kang <68066437+Profile-exe@users.noreply.github.com>
2020-12-06 17:02:19 +0900

    Create 심프 라이브코딩 1차 2장.c

---
+++ just cord/심프 라이브코딩 1차 2장.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	long a, b, integer, decimal = 0;
+	scanf("%ld %ld", &a, &b);
+
+	integer = a;
+	for (int i = 0; i < b; i++) {
+		decimal += (integer % 10) * pow(10, i);
+		integer /= 10;
+	}
+
+	printf("%ld", integer);
+	if (b)
+		printf(".%ld", decimal);
+	return 0;
+}
